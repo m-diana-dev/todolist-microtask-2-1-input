c2a6b8e1fdb57aa0a29c9ade2500e61527b1304c
m-diana-dev <m.diana.dev@gmail.com>
2023-05-03 13:34:22 +0300

    add 2 microtask

--- src/components/FullInput.tsx
+++ src/components/FullInput.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 
 export const FullInput = () => {
+    let [title, setTitle] = useState('')
+    const onChangeInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setTitle(event.currentTarget.value);
+        console.log(title)
+    }
     return (
         <div>
-            <input />
+            <input onChange={onChangeInputHandler}/>
             <button>+</button>
         </div>
     );
